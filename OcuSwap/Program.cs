a577632f7d4bc5d79e8fc5b49f7f492e0c10433b
Burrito <37681398+burritosoftware@users.noreply.github.com>
2021-06-07 19:11:01 -0700

    Dark mode swapping works! Nothing else for now.

--- OcuSwap/Program.cs
+++ OcuSwap/Program.cs
@@ -6,55 +6,137 @@ namespace OcuSwap
     {
         static void Main(string[] args)
         {
-            // Initialize with a title and line, waiting for user to hit a key
-            Console.Title = "OcuSwap";
-            Console.WriteLine("Press any key to go dark :D");
-            Console.ReadKey();
+            // Initialize with title and main menu
 
+            Console.Title = "OcuSwap";
+            Console.WriteLine("OcuSwap is now starting...");
+
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+
+        // This checks if the default Oculus path exists and if so returns that, otherwise asks for the user to input it themselves.
+        static string CheckPaths()
+        {
+            if (System.IO.Directory.Exists(@"C:\Program Files\Oculus"))
+            {
+                return @"C:\Program Files\Oculus";
+            }
+            else
+            {
+                Console.WriteLine("\nLooks like Oculus software isn't installed to the default location.\nPlease enter the location to your Oculus path and press Enter.\nExample: C:\\Program Files\\Oculus");
+                return Console.ReadLine();
+            }
+        }
+
+        static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to OcuSwap!\n\nSelect a task:\n1. Change Home environment (Dark Mode)\n2. Adjust Home horizon intensity\n3. Apply Quest 1 Image to Air Link Panel\n4. Exit (typing anything else will exit as well)\n");
+            Console.Write("Type a number and press Enter: ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    DarkMode();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        static void KillOculus()
+        {
             // Kill Oculus Client before we do anything
+            Console.Clear();
             if (System.Diagnostics.Process.GetProcessesByName("OculusClient").Length > 0)
             {
+                Console.WriteLine("Hey! We see the Oculus app is open.\nWe need to close this to apply the patches, so if you're in VR, take off your headset.\n\nPress any key to have us close the client...");
+                Console.ReadKey();
+                Console.WriteLine("We're closing the Oculus Client so we can do our things...");
                 foreach (var process in System.Diagnostics.Process.GetProcessesByName("OculusClient"))
                 {
                     process.Kill();
                 }
+                Console.Clear();
             }
-
-            string theVoidPath;
-
-            // Checking paths and asking for another path if not found
-            bool pathsExist = CheckPaths();
-            if (pathsExist)
-            {
-                theVoidPath = @"C:\Program Files\Oculus\Support\oculus-dash\dash\assets\raw\textures\environment\the_void";
-            } else
-            {
-                Console.WriteLine("\nLooks like Oculus software isn't installed to the default location.\nPlease enter the location to your Oculus path and press Enter.");
-                theVoidPath = Console.ReadLine();
-            }
-
-            string darkDotDDS = theVoidPath + @"\grid_plane_004.dds";
-            string lightGridDDS = theVoidPath + @"\grid_plane_006.dds";
-
-            // Beginning to overwrite
-            // TODO: Check if we actually have already swapped or not
-
-            Console.WriteLine("Overwriting Dark Grid to Light Grid");
-            System.IO.File.Move(darkDotDDS, lightGridDDS, true);
-            Console.WriteLine("Done! Make sure to start your Oculus app again. Press any key to exit.");
-            Console.ReadKey();
         }
 
-        static bool CheckPaths()
+        static void DarkMode()
         {
-            if (System.IO.Directory.Exists(@"C:\Program Files\Oculus\Support\oculus-dash\dash\assets\raw\textures\environment\the_void"))
+            Console.Clear();
+            string voidPath = CheckPaths() + @"\Support\oculus-dash\dash\assets\raw\textures\environment\the_void";
+            string darkGridDDS = voidPath + @"\grid_plane_007.dds";
+            string darkDotDDS = voidPath + @"\grid_plane_004.dds";
+            string greyDotsDDS = voidPath + @"\grid_plane_003.dds";
+            string lightGridDDS = voidPath + @"\grid_plane_006.dds";
+
+            // Checking if our backup of white grid texture exists
+            string lightGridDDSBackup = lightGridDDS + ".old";
+            if (!System.IO.File.Exists(lightGridDDSBackup))
             {
-                return true;
+                System.IO.File.Copy(lightGridDDS, lightGridDDS + ".old");
             }
-            else
+
+            string selectedDDS;
+            string selectedDDSMessage;
+
+            Console.WriteLine("So, what would you like to change the Oculus Home environment to?\n\n1. Black environment with grid\n2. Black environment with dots (my favorite)\n3. Grey environment with dots\n4. Restore white grid\n5. Back to main menu\n");
+            Console.Write("Type a number and press Enter: ");
+            switch (Console.ReadLine())
             {
-                return false;
+                case "1":
+                    selectedDDS = darkGridDDS;
+                    selectedDDSMessage = "Black environment with grid";
+                    break;
+                case "2":
+                    selectedDDS = darkDotDDS;
+                    selectedDDSMessage = "Black environment with dots";
+                    break;
+                case "3":
+                    selectedDDS = greyDotsDDS;
+                    selectedDDSMessage = "Grey environment with dots";
+                    break;
+                case "4":
+                    selectedDDS = lightGridDDSBackup;
+                    selectedDDSMessage = "Restore white grid";
+                    break;
+                default:
+                    return;
+            }
+
+            Console.Clear();
+            Console.WriteLine("You have selected: " + selectedDDSMessage);
+            Console.WriteLine("Ready to apply?\n\n1. Apply\n2. Back to main menu");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    break;
+                case "2":
+                    return;
+            }
+            KillOculus();
+            Console.WriteLine("Applying your selected environment...");
+            System.IO.File.Copy(selectedDDS, lightGridDDS, true);
+            FinishedPrompt();
+        }
+
+        static void FinishedPrompt()
+        {
+            Console.Clear();
+            Console.WriteLine("We're finished! Open Oculus Home to see your changes.\nWhat would you like to do?\n\n1. Return to main menu\n2. Exit\n");
+            Console.Write("Type a number and press Enter: ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    return;
+                case "2":
+                    System.Environment.Exit(0);
+                    break;
             }
         }
+
     }
 }

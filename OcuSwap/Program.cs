57a33406e57c477d186456ca64142d65aa37a462
Burrito <37681398+burritosoftware@users.noreply.github.com>
2021-06-13 15:21:47 -0700

    Change air link panel added: 0.2.0

--- OcuSwap/Program.cs
+++ OcuSwap/Program.cs
@@ -25,7 +25,7 @@ namespace OcuSwap
         static bool MainMenu()
         {
             Console.Clear();
-            Console.WriteLine("Welcome to OcuSwap!\n\nSelect a task:\n1. Change Home environment (Dark Mode)\n2. Adjust Home horizon intensity\n3. Apply Quest 1 Image to Air Link Panel\n4. Exit (typing anything else will exit as well)\n");
+            Console.WriteLine("Welcome to OcuSwap!\n\nSelect a task:\n1. Change Home environment (Dark Mode)\n2. Adjust Home horizon intensity\n3. Change Air Link panel image (Quest 1)\n4. Exit (typing anything else will exit as well)\n");
             Console.Write("Type a number and press Enter: ");
             switch (Console.ReadLine())
             {
@@ -33,6 +33,8 @@ namespace OcuSwap
                     return DarkMode();
                 case "2":
                     return HorizonChange();
+                case "3":
+                    return AirLinkPanel();
                 default:
                     return false;
             }
@@ -185,8 +187,64 @@ namespace OcuSwap
                     return true;
             }
             KillOculus();
-            File.WriteAllText(voidShaderPath, voidShaderPatched);
             Console.WriteLine("Applying your horizon intensity...");
+            File.WriteAllText(voidShaderPath, voidShaderPatched);
+            return FinishedPrompt();
+        }
+
+        static bool AirLinkPanel()
+        {
+            Console.Clear();
+
+            string panelPath = CheckPaths() + @"\Support\oculus-dash\dash\data\js\assets\src\apps\dash\asset_files\experience-panels\oculus-airlink.png";
+
+            // Checking if our backup of Quest 2 panel exists, if not backs up for restoration
+            string quest2PanelBackup = panelPath + ".old";
+            if (!File.Exists(quest2PanelBackup))
+            {
+                File.Copy(panelPath, quest2PanelBackup);
+            }
+
+            string selectedPanelMessage;
+            bool applyPanel;
+
+            Console.WriteLine("So, what would you like to change the Air Link panel to?\n\n1. Quest 1 image\n2. Restore Quest 2 image\n3. Back to main menu\n");
+            Console.Write("Type a number and press Enter: ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    applyPanel = true;
+                    selectedPanelMessage = "Quest 1 image";
+                    break;
+                case "2":
+                    applyPanel = false;
+                    selectedPanelMessage = "Restore Quest 2 image";
+                    break;
+                default:
+                    return true;
+            }
+
+            Console.Clear();
+            Console.WriteLine("You have selected: " + selectedPanelMessage);
+            Console.WriteLine("Ready to apply?\n\n1. Apply\n2. Back to main menu\n");
+            Console.Write("Type a number and press Enter: ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    break;
+                default:
+                    return true;
+            }
+
+            KillOculus();
+            Console.WriteLine("Applying your panel image...");
+            if (applyPanel)
+            {
+                File.WriteAllBytes(panelPath, OcuSwap.Resources.oculus_airlink);
+            } else
+            {
+                File.Copy(quest2PanelBackup, panelPath, true);
+            }
             return FinishedPrompt();
         }
 
